6788a02de21215e6349bcc8fc06e512933f1a742
Nepal Singh <nepalsss007@gmail.com>
2023-11-15 13:07:55 +0530

    more errors

--- src/components/Mainlist.js
+++ src/components/Mainlist.js
@@ -1,17 +1,19 @@
-import React from 'react'
+import React from 'react';
 
 export default function Mainlist(props) {
   const Addfav = props.Addfav;
-  return (
-      <>
-      <div className="sub-heading">{props.heading}</div>
-    <div className='movies-container'>
-        {props.movies && props.movies.map((movie , index )=><div className='image-container '>
-            <img className='movie-poster' src={movie.Poster}  alt="movie" />
-            <div className="overlay" onClick={() => props.setfav(movie)}> <Addfav/> </div>
-        </div>
 
-        )}
-    </div></>
-  )
+  return (
+    <>
+      <div className="sub-heading">{props.heading}</div>
+      <div className='movies-container'>
+        {props.movies && props.movies.map((movie, index) => (
+          <div className='image-container' key={index}>
+            <img className='movie-poster' src={movie.Poster} alt="movie" />
+            <div className="overlay" onClick={() => props.setfav(movie)}> <Addfav /> </div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
 }
